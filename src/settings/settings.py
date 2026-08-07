04fa86acb185731c688cfc583e71939bc7a13d31
Jean-Francois Labonte <jeff.labonte12@gmailcom>
2026-08-07 17:04:19 -0400

    settings: make STATIC_ROOT configurable via DJANGO_STATIC_ROOT

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -163,5 +163,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = Path(os.environ.get('DJANGO_STATIC_ROOT', str(BASE_DIR / 'staticfiles')))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
